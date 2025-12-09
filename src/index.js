95bc769575c2e150aa1f5588b82f954d73085ccb
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-12-09 15:01:24 -0800

    restructured directories for better organization

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import "./styles.css";
 
+import DropDownMenu from "./modules/DropdownMenu";
 
-const dropDownMenu = document.querySelector(".dropdown-menu")
-dropDownMenu.style.visibility = 'hidden';
-
-const dropDownMenuCreator = () => {
-    const dropdownBtn = document.querySelector("#dropdown-btn");
-    const dropDownMenu = document.querySelector(".dropdown-menu")
-    dropDownMenu.setAttribute("visibility", "hidden");
-    dropdownBtn.addEventListener("click", () => {
-        if (dropDownMenu.style.visibility === 'hidden')
-            dropDownMenu.style.visibility = 'visible';
-        else
-            dropDownMenu.style.visibility = 'hidden'; 
-
-    })
-}
-
-dropDownMenuCreator();
+DropDownMenu();
 
